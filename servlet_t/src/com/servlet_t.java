1b8c6100855bac75954e028ced1ace2e3ff84963
盐焗鸡YanJJ <yanrunzhi@126.com>
2019-10-15 09:52:10 +0800

    Add files via upload

---
+++ servlet_t/src/com/servlet_t.java
@@ -0,0 +1,28 @@
+package com;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+@WebServlet(name = "TestServlet")
+public class TestServlet extends HttpServlet{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out=response.getWriter();
+        Date today = new Date();
+        out.println("<html>");
+        out.println("<head><title>servlet test</title></head>");
+        out.println("<body><h1>hello TestServlet</h1>");
+        out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(today));
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
